ba921e4610cf9883251d645b4dac9b31dccf4ab3
bronsilber <bron.teixeira.s@gmail.com.br>
2022-07-12 14:06:26 -0300

    resolvendo bugs do firestore

--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+    Widget _builderBodyback() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255,0,255,255),
+            Color.fromARGB(255,0,128,128)
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft
+        )
+      ),
+    );
+
+    return Stack( 
+      children: [
+        _builderBodyback(),
+        CustomScrollView(
+          slivers: <Widget>[
+             SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text("Novidades"),
+                centerTitle: true
+              ),
+             ),
+             FutureBuilder<QuerySnapshot> (
+              future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
+              builder: (context,  snapshot) {
+                if(!snapshot.hasData) return SliverToBoxAdapter(
+                  child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white)
+                      ),
+                    )
+                  );
+                  else {
+                    print(snapshot.data.documents.length);
+                    return SliverToBoxAdapter(
+                  child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: Container()
+                    )
+                  );
+                  }
+                },
+             )
+          ]
+        ) 
+      ],
     );
   }
 }
