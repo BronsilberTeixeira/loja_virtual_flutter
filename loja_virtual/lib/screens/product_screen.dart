fbea05651fe2e5c62a409d3c87589194304b3d96
bronsilber <bron.teixeira.s@gmail.com.br>
2022-08-10 11:30:29 -0300

    Tela de produto

---
+++ loja_virtual/lib/screens/product_screen.dart
@@ -0,0 +1,135 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/products_data.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+class ProductScreen extends StatefulWidget {
+
+  final ProductData productData;
+
+  ProductScreen(this.productData);
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState(productData);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+
+  String size;
+
+  final ProductData productData;
+  _ProductScreenState(this.productData);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(productData.title, style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: productData.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 5.0,
+              dotSpacing: 10,
+              dotBgColor: Color.fromARGB(50, 255, 255, 255),
+              dotColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(productData.title,
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${productData.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Text(
+                  "Tamanho", 
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5
+                    ),
+                    children: productData.sizes.map(
+                      (s) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              size = s; 
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              border: Border.all(
+                                color: s == size ? Colors.blue[400] : 
+                                  primaryColor,
+                                width: 2
+                              )
+                            ),
+                            width: 50,
+                            alignment: Alignment.center,
+                            child: Text(s, style: TextStyle(
+                              color: s == size ? Colors.blue[400] : 
+                                  primaryColor
+                            ),),
+                          ),
+                        );
+                      }
+                    ).toList(),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                SizedBox(
+                  height: 45,
+                  child: RaisedButton(
+                    onPressed: size != null ?
+                    () {} : null,
+                    child: Text('Adicionar ao carrinho',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: size != null ? 
+                          Colors.blue[400] : Colors.grey[400]
+                      ),),
+                    color: Colors.white,
+                  ),
+                ),              
+              ],
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
--- loja_virtual/lib/tiles/product_tile.dart
+++ loja_virtual/lib/tiles/product_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/products_data.dart';
+import 'package:loja_virtual/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
 
@@ -11,6 +12,11 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProductScreen(productData))
+        );
+      },
       child: Card(
         child: type == "grid" ? 
         Column(
@@ -45,7 +51,39 @@ class ProductTile extends StatelessWidget {
             )
           ],
         ) 
-        : Row(),
+        : Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                productData.images[0],
+                fit: BoxFit.cover,
+                height: 200,
+              )
+            ),
+            Padding(padding: EdgeInsets.only(left: 10)),
+            Flexible(
+              flex: 1,
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget >[
+                    Text(productData.title, 
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Text("R\$ ${productData.price.toStringAsFixed(2)}", 
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       )
     );
   }
